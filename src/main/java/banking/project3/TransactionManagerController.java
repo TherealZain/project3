24711a51fd28c4b2503d8abc88c2531776f3f148
Zain Zulfiqar <zzainzulfiqar@gmail.com>
2023-10-26 15:55:04 -0400

    Change layout

--- src/main/java/banking/project3/TransactionManagerController.java
+++ src/main/java/banking/project3/TransactionManagerController.java
@@ -1,16 +1,42 @@
 package banking.project3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import javafx.fxml.FXML;
 
 public class TransactionManagerController {
     @FXML
-    private Label welcomeText;
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private DatePicker dob;
+    @FXML
+    private RadioButton checking;
+    @FXML
+    private RadioButton collegeChecking;
+    @FXML
+    private RadioButton savings;
+    @FXML
+    private RadioButton moneyMarket;
+    @FXML
+    private Button open;
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void openAccount(ActionEvent event) {
+        try {
+            String firstNameString = firstName.getText();
+            String lastNameString = lastName.getText();
+            Date accountDob = new Date(dob.getValue().getYear(),
+                    dob.getValue().getMonthValue(), dob.getValue().getDayOfMonth());
+        }
+        catch(NumberFormatException e){
+            System.out.println("error" + e);
+        }
+
     }
 }
